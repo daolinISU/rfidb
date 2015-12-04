72f09e4b558f65f152005cfa4860dcef711249aa
Daolin Cheng <dlcheng@iastate.edu>
2015-12-04 15:14:46 -0600

    link for navigator button RFIDB - 2

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -62,7 +62,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">RFIDB</a>
+            <a class="navbar-brand" href="/#">RFIDB</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
